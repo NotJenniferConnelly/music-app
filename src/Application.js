b64e1dbd477b64f49985dd7eb0fbba61b03b549a
NotJenniferConnelly <NotJenniferConnelly@protonmail.com>
2021-04-22 20:40:32 -0400

    rearranged files

---
+++ src/Application.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react"
+
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(270deg, #000, ${props => props.color} 50%, #000 100%)
+`
+
+const Application = () => {
+  const [color, setColor] = useState("#9f3a")
+
+  const getRandomNumber = () => Math.floor(Math.random() * 10)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setColor(
+        '#' + getRandomNumber() + getRandomNumber() + getRandomNumber()
+      )
+    }, 5000)
+
+    return () => { clearInterval(interval) }
+  }, [])
+
+  return (
+    <Container color={color}>
+    </Container>
+  )
+}
+
+export default Application
